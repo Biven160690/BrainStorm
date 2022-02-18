6cd3adbd726bd9ec0c1a327d91c41666477619dd
Biven160690 <bivenmira1990@gmail.com>
2022-02-18 10:05:35 +0300

    changed-files-export

--- src/components/blocks/Footer.tsx
+++ src/components/blocks/Footer.tsx
@@ -1,19 +1,17 @@
-import { Typography, Box } from '@mui/material';
+import { Typography, Box } from "@mui/material";
 
-import { useStyles } from '../../theme/style';
+import { useStyles } from "../../theme/style";
 
-const Footer = () => {
-  const { footer, footerContext } = useStyles()
+export const Footer = () => {
+  const { footer, footerContext } = useStyles();
 
   return (
     <footer className={footer}>
       <Box className={footerContext}>
-        <Typography component="p" color="text.secondary">
+        <Typography component="p" color="text.secondary" role="paragraph">
           © {new Date().getFullYear()}
         </Typography>
       </Box>
     </footer>
-  )
-}
-
-export default Footer
+  );
+};
